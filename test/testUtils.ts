bdb33cdd66a7e0fd1534f0a15ab8b3dcf95f7762
Davy Hausser <davy.hausser@gmail.com>
2021-06-03 17:48:34 +0200

    initial commit

--- test/testUtils.ts
+++ test/testUtils.ts
@@ -1,20 +1,11 @@
-import { render } from '@testing-library/react'
-// import { ThemeProvider } from "my-ui-lib"
-// import { TranslationProvider } from "my-i18n-lib"
-// import defaultStrings from "i18n/en-x-default"
+import { FC, ReactElement } from 'react'
+import { render, RenderResult } from '@testing-library/react'
 
-const Providers = ({ children }) => {
+const Providers: FC<any> = ({ children }) => {
   return children
-  // return (
-  //   <ThemeProvider theme="light">
-  //     <TranslationProvider messages={defaultStrings}>
-  //       {children}
-  //     </TranslationProvider>
-  //   </ThemeProvider>
-  // )
 }
 
-const customRender = (ui, options = {}) =>
+const customRender = (ui: ReactElement, options = {}): RenderResult =>
   render(ui, { wrapper: Providers, ...options })
 
 // re-export everything
