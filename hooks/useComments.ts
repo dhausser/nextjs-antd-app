228e897757dfe341b76eb2315c8f25b0dee180c2
Davy Hausser <davy.hausser@gmail.com>
2021-06-03 17:47:52 +0200

    initial commit

---
+++ hooks/useComments.ts
@@ -0,0 +1,19 @@
+import useSWR from 'swr'
+import fetcher from 'utils'
+import db from '../db.json'
+
+type Comment = typeof db.comments[0]
+
+const url = 'http://localhost:4000/comments'
+
+function useComments() {
+  const { data, error } = useSWR<Comment[]>(url, fetcher)
+
+  return {
+    comments: data,
+    isLoading: !error && !data,
+    isError: error,
+  }
+}
+
+export default useComments
