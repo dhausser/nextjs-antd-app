b8aab16174250b64c20d56c5230ab2e9ffa42e4c
Davy Hausser <davy.hausser@gmail.com>
2021-06-03 17:47:42 +0200

    initial commit

---
+++ components/EditableTable.tsx
@@ -0,0 +1,211 @@
+import React, { useContext, useState, useEffect, useRef } from 'react'
+import { Table, Input, Button, Popconfirm, Form } from 'antd'
+import { FormInstance } from 'antd/lib/form'
+
+const EditableContext = React.createContext<FormInstance<any> | null>(null)
+
+interface Item {
+  key: string
+  name: string
+  age: string
+  address: string
+}
+
+interface EditableRowProps {
+  index: number
+}
+
+const EditableRow: React.FC<EditableRowProps> = ({ index, ...props }) => {
+  const [form] = Form.useForm()
+  return (
+    <Form form={form} component={false}>
+      <EditableContext.Provider value={form}>
+        <tr {...props} />
+      </EditableContext.Provider>
+    </Form>
+  )
+}
+
+interface EditableCellProps {
+  title: React.ReactNode
+  editable: boolean
+  children: React.ReactNode
+  dataIndex: keyof Item
+  record: Item
+  handleSave: (record: Item) => void
+}
+
+const EditableCell: React.FC<EditableCellProps> = ({
+  title,
+  editable,
+  children,
+  dataIndex,
+  record,
+  handleSave,
+  ...restProps
+}) => {
+  const [editing, setEditing] = useState(false)
+  const inputRef = useRef<Input>(null)
+  const form = useContext(EditableContext)!
+
+  useEffect(() => {
+    if (editing) {
+      inputRef.current!.focus()
+    }
+  }, [editing])
+
+  const toggleEdit = () => {
+    setEditing(!editing)
+    form.setFieldsValue({ [dataIndex]: record[dataIndex] })
+  }
+
+  const save = async () => {
+    try {
+      const values = await form.validateFields()
+
+      toggleEdit()
+      handleSave({ ...record, ...values })
+    } catch (errInfo) {
+      console.log('Save failed:', errInfo)
+    }
+  }
+
+  let childNode = children
+
+  if (editable) {
+    childNode = editing ? (
+      <Form.Item
+        style={{ margin: 0 }}
+        name={dataIndex}
+        rules={[
+          {
+            required: true,
+            message: `${title} is required.`,
+          },
+        ]}
+      >
+        <Input ref={inputRef} onPressEnter={save} onBlur={save} />
+      </Form.Item>
+    ) : (
+      <div
+        className="editable-cell-value-wrap"
+        style={{ paddingRight: 24 }}
+        onClick={toggleEdit}
+      >
+        {children}
+      </div>
+    )
+  }
+
+  return <td {...restProps}>{childNode}</td>
+}
+
+interface DataType {
+  id: number
+  title: string
+  author: string
+}
+
+type EditableTableProps = Parameters<typeof Table>[0] & {
+  setDataSource: (arg: DataType[]) => void
+  newData: DataType
+}
+
+type ColumnTypes = Exclude<EditableTableProps['columns'], undefined>
+
+function EditableTable({
+  dataSource,
+  setDataSource,
+  newData,
+}: EditableTableProps) {
+  const [items, setItems] = useState<DataType[] | null>(null)
+  const columns: (ColumnTypes[number] & {
+    editable?: boolean
+    dataIndex: string
+  })[] = [
+    {
+      title: 'Title',
+      dataIndex: 'title',
+      width: '30%',
+      editable: true,
+    },
+    {
+      title: 'Author',
+      dataIndex: 'author',
+    },
+    {
+      title: 'operation',
+      dataIndex: 'operation',
+      render: (_, record) =>
+        dataSource != null && dataSource.length >= 1 ? (
+          <Popconfirm
+            title="Sure to delete?"
+            onConfirm={() => handleDelete(record.id)}
+          >
+            <a>Delete</a>
+          </Popconfirm>
+        ) : null,
+    },
+  ]
+
+  const handleDelete = (key: React.Key) => {
+    if (!items) return
+    setItems(items.filter((item: DataType) => item.id !== key))
+  }
+
+  const handleAdd = () => {
+    if (!items) return
+    setItems([...items, newData])
+  }
+
+  const handleSave = (row: DataType) => {
+    if (!items) return
+    const newData = [...items]
+    const index = newData.findIndex((item) => row.id === item.id)
+    const item = newData[index]
+    newData.splice(index, 1, {
+      ...item,
+      ...row,
+    })
+    setDataSource(newData)
+  }
+
+  const components = {
+    body: {
+      row: EditableRow,
+      cell: EditableCell,
+    },
+  }
+  const reducedColumns = columns.map((col) => {
+    if (!col.editable) {
+      return col
+    }
+    return {
+      ...col,
+      onCell: (record: DataType) => ({
+        record,
+        editable: col.editable,
+        dataIndex: col.dataIndex,
+        title: col.title,
+        handleSave,
+      }),
+    }
+  })
+
+  return (
+    <div>
+      <Button onClick={handleAdd} type="primary" style={{ marginBottom: 16 }}>
+        Add a row
+      </Button>
+      <Table
+        components={components}
+        rowClassName={() => 'editable-row'}
+        bordered
+        dataSource={dataSource}
+        columns={reducedColumns as ColumnTypes}
+      />
+    </div>
+  )
+}
+
+export default EditableTable
