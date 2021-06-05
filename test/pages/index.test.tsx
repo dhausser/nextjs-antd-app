6af08fae7b266b494ac37a12b1ec438b36912a84
Davy Hausser <davy.hausser@gmail.com>
2021-06-05 14:04:58 +0200

    match eslint-vonfig-jest rules

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import { render, fireEvent } from '../testUtils'
+import { render, screen, fireEvent } from '../testUtils'
 import Home from '../../pages/index'
 
-describe('Home page', () => {
+describe('something', () => {
   it('matches snapshot', () => {
+    expect.hasAssertions()
     const { asFragment } = render(<Home />, {})
     expect(asFragment()).toMatchSnapshot()
   })
 
   it('clicking button triggers alert', () => {
-    const { getByText } = render(<Home />, {})
-    window.alert = jest.fn()
-    fireEvent.click(getByText('Test Button'))
+    expect.hasAssertions()
+    render(<Home />)
+    jest.spyOn(window, 'alert').mockImplementation()
+    fireEvent.click(screen.getByText('Test Button'))
     expect(window.alert).toHaveBeenCalledWith('With typescript and Jest')
   })
 })
