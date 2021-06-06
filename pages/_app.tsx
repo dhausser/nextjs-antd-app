f09aeae9032c885603eda8212e5c26015b2f887a
Davy Hausser <davy.hausser@gmail.com>
2021-06-06 15:44:52 +0200

    change to light theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 import { Layout } from 'components/Layout'
-import 'antd/dist/antd.dark.css'
+import 'antd/dist/antd.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
