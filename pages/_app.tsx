16618616e50d48e296da4f3aab9a56458eabd32b
Davy Hausser <davy.hausser@gmail.com>
2021-06-12 13:11:26 +0200

    Edit app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import type { AppProps } from 'next/app'
-import { Layout } from 'components/Layout'
-import 'antd/dist/antd.css'
+import Layout from 'components/Layout'
+import 'antd/dist/antd.dark.css'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <Component />
     </Layout>
   )
 }
